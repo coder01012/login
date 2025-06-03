cccd76d1b81f2cea7ef3366a32d0ecfa4d1bcdca
coder01012 <mhmdqnsh097@gmail.com>
2025-06-03 11:33:59 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,20 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "ضع-مفتاح-API-هنا",
-  authDomain: "اسم-المشروع.firebaseapp.com",
-  projectId: "معرّف-المشروع",
-  appId: "معرّف-التطبيق"
+  apiKey: "AIzaSyDvMLI4QL90kIbjVn_6e02dUIa4K1dt3Ws",
+  authDomain: "login-c8afc.firebaseapp.com",
+  projectId: "login-c8afc",
+  storageBucket: "login-c8afc.firebasestorage.app",
+  messagingSenderId: "907378152010",
+  appId: "1:907378152010:web:587c0777c4926b0ba0ccb3"
 };
 
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
